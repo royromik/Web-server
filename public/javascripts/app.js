7208d6f74baf09186f628d518d6e4978bd37be33
royromik <royromik@gmail.com>
2020-12-25 09:27:40 +0530

    all set up to deploy

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,21 +1,23 @@
-console.log('all ok');
-
 const form = document.querySelector('#searchForm');
 const address = document.querySelector('#textInput');
+const weatherInfo = document.querySelector('.weather-info');
+const locationInfo = document.querySelector('.location-info');
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
-    const weatherInfo = document.querySelector('.weather-info');
-    weatherInfo.innerHTML='';
+    locationInfo.classList.remove('red');
+    locationInfo.textContent = 'Loading ..'
+    weatherInfo.textContent='';
     fetch(`http://localhost:3002/weather?address=${address.value}`).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
-            weatherInfo.classList.add('red')
-            weatherInfo.innerHTML = `${data.error}`
+            locationInfo.classList.add('red')
+            locationInfo.textContent = `${data.error}`
             
         } else{
-            weatherInfo.classList.remove('red');
-            weatherInfo.innerHTML = `${data.forecast} ${data.location}`
+            locationInfo.classList.remove('red');
+            locationInfo.textContent = `${data.location}`
+            weatherInfo.textContent = `${data.forecast}`
         }
     })
 })
--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res)=>{
     res.render('index',{
-        title: 'Home page',
+        title: 'Weather App',
         name: 'Romik Roy'
     })
 })
